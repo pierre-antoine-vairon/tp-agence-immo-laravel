e233f825955713845153c745bbf55b6eb31ba301
p-antoinev <pavrnsrf@gmail.com>
2023-06-07 16:58:19 +0200

    Form Blade

--- resources/views/admin/properties/form.blade.php
+++ resources/views/admin/properties/form.blade.php
@@ -5,12 +5,29 @@
 @section('content')
     <h1>@yield('title')</h1>
 
-    <form action="{{ route($property->exists ? 'admin.property.update' : 'admin.property.store',['property' => $property]) }}" method="post">
+    <form class="vstack gap-2" action="{{ route($property->exists ? 'admin.property.update' : 'admin.property.store',['property' => $property]) }}" method="post">
 
         @csrf
         @method($property->exists ? 'put' : 'post')
 
-        @include('shared.input', ['label' => 'Titre', 'name' => 'title', 'value' => $property->title])
+        <div class="row">
+            @include('shared.input', ['class' => 'col', 'label' => 'Titre', 'name' => 'title', 'value' => $property->title])
+            <div class="col row">
+                @include('shared.input', ['class' => 'col', 'name' => 'surface', 'value' => $property->surface])
+                @include('shared.input', ['class' => 'col', 'label' => 'Prix', 'name' => 'price', 'value' => $property->price])
+            </div>
+        </div>
+        @include('shared.input', ['type' => 'textarea', 'name' => 'description', 'value' => $property->description])
+        <div class="row">
+            @include('shared.input', ['class' => 'col', 'name' => 'rooms', 'label' => 'Pièces', 'value' => $property->rooms])
+            @include('shared.input', ['class' => 'col', 'name' => 'bedrooms', 'label' => 'Chambre', 'value' => $property->bedrooms])
+            @include('shared.input', ['class' => 'col', 'name' => 'floor', 'label' => 'Étages', 'value' => $property->floor])
+        </div>
+        <div class="row">
+            @include('shared.input', ['class' => 'col', 'name' => 'adress', 'label' => 'Adresse', 'value' => $property->adress])
+            @include('shared.input', ['class' => 'col', 'name' => 'bedrooms', 'city' => 'Ville', 'value' => $property->city])
+            @include('shared.input', ['class' => 'col', 'name' => 'postal_code', 'label' => 'Code postal', 'value' => $property->postal_code])
+        </div>
 
         <div>
             <button class="btn btn-primary">
--- resources/views/shared/input.blade.php
+++ resources/views/shared/input.blade.php
@@ -1,14 +1,18 @@
 @php
-   $label ??= null;
    $type ??= 'text';
    $class ??= null;
    $name ??= '';
    $value ??= '';
+   $label ??= ucfirst($name);
 @endphp
 
 <div @class(["form-group", $class])>
     <label for="{{ $name }}">{{ $label }}</label>
-    <input class="form-control @error($name) is-invalid @enderror" type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ old($name), $value}}">
+    @if($type == 'textarea')
+        <textarea class="form-control @error($name) is-invalid @enderror" type="{{ $type }}" id="{{ $name }}" name="{{ $name }}">{{ old($name), $value}}</textarea>
+    @else
+        <input class="form-control @error($name) is-invalid @enderror" type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ old($name), $value}}">
+    @endif
     @error($name)
         <div class="invalid-feedback">
             {{ $message }}
